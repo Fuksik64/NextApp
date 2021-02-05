bb02721ebdc700543fff54ae59ad71cf006548b3
Fuksik64 <61504720+Fuksik64@users.noreply.github.com>
2021-02-05 12:28:30 +0100

    05.02-end

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,9 +1,32 @@
-const Ninjas = () => {
+import Link from "next/link";
+import styles from "../../styles/Ninjas.module.css";
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return {
+    props: { ninjas: data },
+  };
+};
+
+const Ninjas = ({ ninjas }) => {
   return (
     <div>
       <h1>All Ninjas</h1>
+      {ninjas.map((ninja) => (
+        <div key={ninja.id}>
+          <Link href={"/ninjas/" + ninja.id}>
+            <a className={styles.single}>
+              <h3>{ninja.name}</h3>
+            </a>
+          </Link>
+        </div>
+      ))}
     </div>
   );
 };
 
 export default Ninjas;
+
+//GetStaticProps runs before component is rendered thats why we can acces props
