2c878c926d03e75b693269db7c42adaf3c316fac
Fuksik64 <61504720+Fuksik64@users.noreply.github.com>
2021-02-05 08:45:13 +0100

    05.02

--- pages/about.js
+++ pages/about.js
@@ -1,23 +1,30 @@
+import Head from "next/head";
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere
-        dignissimos quaerat itaque temporibus repellat laboriosam sint quam
-        dicta pariatur et!
-      </p>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere
-        dignissimos quaerat itaque temporibus repellat laboriosam sint quam
-        dicta pariatur et!
-      </p>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere
-        dignissimos quaerat itaque temporibus repellat laboriosam sint quam
-        dicta pariatur et!
-      </p>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere
+          dignissimos quaerat itaque temporibus repellat laboriosam sint quam
+          dicta pariatur et!
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere
+          dignissimos quaerat itaque temporibus repellat laboriosam sint quam
+          dicta pariatur et!
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere
+          dignissimos quaerat itaque temporibus repellat laboriosam sint quam
+          dicta pariatur et!
+        </p>
+      </div>
+    </>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,32 @@
 import Head from "next/head";
 import Link from "next/link";
-import Footer from "../includes/Footer";
-import Navbar from "../includes/Navbar";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Homepage</h1>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem
-        repellat placeat consequatur natus animi numquam fuga ex reiciendis unde
-        iure. Quos consequatur eveniet magni reiciendis enim ex repudiandae
-        incidunt earum.
-      </p>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente non
-        aliquam nam beatae tempora iste officiis velit quos dolores fugit
-        voluptatum mollitia odit eius incidunt excepturi eaque, cum tempore
-        culpa.
-      </p>
-      <Link href="/ninjas">
-        <a>See Ninja Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>{" "}
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem
+          repellat placeat consequatur natus animi numquam fuga ex reiciendis
+          unde iure. Quos consequatur eveniet magni reiciendis enim ex
+          repudiandae incidunt earum.
+        </p>
+        <p className={styles.text}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente non
+          aliquam nam beatae tempora iste officiis velit quos dolores fugit
+          voluptatum mollitia odit eius incidunt excepturi eaque, cum tempore
+          culpa.
+        </p>
+        <Link href="/ninjas">
+          <a className={styles.btn}>See Ninja Listing</a>
+        </Link>
+      </div>
+    </>
   );
 }
